f81c5ecaa0770d8e7f4461b558bf3ea98abb89ae
16022582 <16022582@myrp.edu.sg>
2018-05-07 13:17:23 +0800

    button on click

--- app/src/main/java/com/example/a16014753/p03_classjournal/InfoForC347.java
+++ app/src/main/java/com/example/a16014753/p03_classjournal/InfoForC347.java
@@ -1,13 +1,28 @@
 package com.example.a16014753.p03_classjournal;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class InfoForC347 extends AppCompatActivity {
+
+    int requestCodeForAdd = 1;
+    Button btnAddData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+
+        btnAddData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(InfoForC347.this,
+                        AddData.class);
+                startActivityForResult(i,requestCodeForAdd);
+            }
+        });
     }
 }
