b072343e1b92ac4bd060cae073ece55223309fd5
16014753 <16014753@myrp.edu.sg>
2018-05-07 13:14:13 +0800

    Added intent for MainActivity to InfoForC347

--- app/src/main/java/com/example/a16014753/p03_classjournal/MainActivity.java
+++ app/src/main/java/com/example/a16014753/p03_classjournal/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.a16014753.p03_classjournal;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvC347;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvC347 = (TextView)findViewById(R.id.textViewC347);
+
+        tvC347.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, InfoForC347.class);
+                startActivity(i);
+            }
+        });
 
     }
 }
