08e16baf3a8e52acb1491825bebf74b4ecb1460b
16023068 <tfabiyus@gmail.com>
2018-05-07 14:56:05 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/java/com/example/a16014753/p03_classjournal/InfoForC347.java

--- app/src/main/java/com/example/a16014753/p03_classjournal/InfoForC347.java
+++ app/src/main/java/com/example/a16014753/p03_classjournal/InfoForC347.java
@@ -1,6 +1,7 @@
 package com.example.a16014753.p03_classjournal;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -15,12 +16,14 @@ Button email;
     ListView lv;
     ArrayAdapter aa;
     ArrayList<Week> week;
+    Button web;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
 
         lv = this.findViewById(R.id.listViewWeek);
+        web = (Button)findViewById(R.id.buttonInfo);
         week = new ArrayList<Week>();
         week.add(new Week("Week 1", "B"));
         week.add(new Week("Week 2", "C"));
@@ -41,6 +44,14 @@ Button email;
                 startActivity(Intent.createChooser(email,
                         "Choose an Email client :"));
 
+            }
+        });
+        web.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent webIntent = new Intent (Intent.ACTION_VIEW);
+                webIntent.setData(Uri.parse("http://www.rp.edu.sg"));
+                startActivity(webIntent);
             }
         });
     }
