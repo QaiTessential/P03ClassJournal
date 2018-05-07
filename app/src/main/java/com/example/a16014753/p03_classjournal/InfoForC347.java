21389225cef7ad047fa93329e423e5bb2ed0824f
16014753 <16014753@myrp.edu.sg>
2018-05-07 12:49:19 +0800

    Added buttons for activities

--- app/src/main/java/com/example/a16014753/p03_classjournal/InfoForC347.java
+++ app/src/main/java/com/example/a16014753/p03_classjournal/InfoForC347.java
@@ -3,12 +3,11 @@ package com.example.a16014753.p03_classjournal;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class InfoForC347 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        setContentView(R.layout.activity_info);
     }
 }
