1f631f13663b3b19f28cbdc602d19a50e218893b
16023068 <tfabiyus@gmail.com>
2018-05-07 13:19:17 +0800

    nono

--- app/src/main/java/com/example/a16014753/p03_classjournal/InfoForC347.java
+++ app/src/main/java/com/example/a16014753/p03_classjournal/InfoForC347.java
@@ -1,13 +1,30 @@
 package com.example.a16014753.p03_classjournal;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class InfoForC347 extends AppCompatActivity {
-
+Button email;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
+        email = (Button) findViewById(R.id.buttonEmail);
+
+        email.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent email = new Intent (Intent.ACTION_SEND);
+                email.putExtra(Intent.EXTRA_EMAIL,new String[]{"jason_lim@rp.edu.sg"});
+                email.putExtra(Intent.EXTRA_TEXT,"Hi Faci, /n /n I am... /n Please see my remarks so far, thank you!");
+                email.setType("message/rfc822");
+                startActivity(Intent.createChooser(email,
+                        "Choose an Email client :"));
+
+            }
+        });
     }
 }
